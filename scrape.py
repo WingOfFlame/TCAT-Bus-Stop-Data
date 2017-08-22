ff78735494e26f4ddf19021a2fc49f8e72b33b1a
Justin Hu <j62hu@uwaterloo.ca>
2017-08-21 23:46:03 -0400

    Fetch data from https://realtimetcatbus.availtec.com/InfoPoint/

--- scrape.py
+++ scrape.py
@@ -1,71 +1,103 @@
 from bs4 import BeautifulSoup
-from urllib.request import urlopen
-from urllib.parse import urlparse, parse_qsl
+import requests
+import json
 
-class Stop:
-
-	def __init__(self, name, stopid, area, zone, addl, lat, lon):
-		self.name = name
-		self.id = stopid
-		self.area = area
-		self.zone  = zone
-		self.addl = addl
-		self.lat = lat
-		self.lon = lon
-
-	def toCSV(self):
-		self.name = '"' + self.name.replace('"','""') + '"' #wrap in quotes and escape '"''
-		self.addl = '"' + self.addl.replace('"','""') + '"'
-		return("{},{},{},{},{},{},{}\n".format(self.id,self.name,self.zone,self.lat,self.lon,self.area,self.addl))
-
-#-------------------------------------------------
-stops = [] #container for stop objects
-BASE_URL = "http://tcat.nextinsight.com"
-urlcontents = urlopen(BASE_URL+"/allstops.php")
-allstops = BeautifulSoup(urlcontents)
-
-links = allstops.find(id="page").find("p").find_all("a")
-
-print("Found {} stops. Starting data collection.".format(len(links)))
-for link in links:
-	#extract useful info from the link
-	stopurl = link.get("href")
-	print("Collecting data in: " + stopurl)
-
-	name = link.get_text()
-	
-	#visit the stop link
-	urlcontents = urlopen(BASE_URL + stopurl)
-	stop = BeautifulSoup(urlcontents)
-	area = stop.find("h4",text="Area").find_next_sibling().get_text()
-	zone = stop.find("h4", text="Fare Zone").find_next_sibling().get_text()
-	zone = zone.replace("Zone ","") #strip Zone prefix
-	addl = stop.find("h4", text="Additional Information").find_next_sibling().get_text()
-	addl = addl.replace("\n","") #strip newlines
-	mapurl = stop.find("a", text="View Stop On Map").get("href") #?lat=42.393639&lon=-76.362834&stopid=9368
-	mapquery = parse_qsl(urlparse(mapurl).query)
-	
-	#[('lat', '42.458150'), ('lon', '-76.475667'), ('stopid', '3660')]
-	lat = mapquery[0][1]
-	lon = mapquery[1][1]
-	stopid = mapquery[2][1]
-
-	#create the stop object
-	stops.append(Stop(name, stopid, area, zone, addl, lat, lon))
-
-print("Data collection finished.\nCollected {} stops.\nBegin writing to file.".format(len(stops)))
+from pathlib import Path
 
 
-f = open('TCATstops.csv', 'w')
-f.write("id,name,zone,lat,lon,area,addl\n")
 
-#iterate through stop objects
-for stop in stops:
-	f.write(stop.toCSV())
+BASE_URL = "https://realtimetcatbus.availtec.com/InfoPoint/Minimal"
+STOPS_BY_ROUTE = "/Stops/ForRoute?routeId="
+TIMES_BY_STOP = "/Departures/ForStop?stopId="
 
-f.close()
-print("Completed")
+def parse_route_colors():
+    colors = {}
+
+    with open('color.html', 'r') as f:
+        page = BeautifulSoup(f.read(), 'html.parser')
+
+        colors_raw = page.find_all('div', class_='route-abbr')
+        for c in colors_raw:
+            code = (c.get('style').split(';')[0]).split(':')[1].strip()
+            route = c.text.split(' ')[1].strip()
+            colors[route]=code
+
+    return colors
+
+def fetch_routes():
+    r = requests.get(BASE_URL)
+    routes = []
+
+    if r.status_code != 200:
+        return routes
+
+    page = BeautifulSoup(r.text, 'html.parser')
+    routes_raw = page.find_all('a')[0:-1]
+
+    print("%d routes found" % (len(routes_raw)))
+    for route in routes_raw:
+        route_dict = {'id': route.get('routeid'), 'name': route.text, 'color':COLORS_DICT[route.get('routeid')]} #'href': route.get('href'),
+        routes.append(route_dict)
+
+    return routes
+
+def fetch_stops_by_route(routeid):
+    r = requests.get(BASE_URL + STOPS_BY_ROUTE + routeid)
+    stops = {}
+
+    if r.status_code != 200:
+        return stops
+
+    page = BeautifulSoup(r.text, 'html.parser')
+    stops_raw = page.find_all('a')[0:-1][1::2]  # only cares every second line
+
+    print("%d stops found for route %s" %(len(stops_raw), routeid))
+    for stop in stops_raw:
+        stops[stop.get('stopid')] = stop.text
+
+    return stops
+
+def fetch_stops():
+    route_stop_dict={}
+    stops_dict={}
+
+    for r in ROUTES:
+        stops = fetch_stops_by_route(r['id'])
+        route_stop_dict[r['id']]=list(stops.keys())
+        stops_dict.update(stops)
+
+    stops=[]
+    for key in sorted(stops_dict.keys()):
+        stops.append({'id':key, 'name': stops_dict[key]})
+
+    print("%d stops found"%(len(stops)))
+    return route_stop_dict, stops
 
 
+''' Getting local/manual data '''
+# color code for routes
+my_file = Path("./color.json")
+if not my_file.is_file():
+    COLORS_DICT = parse_route_colors()
+    with open('color.json', 'w') as fp:
+        json.dump(COLORS_DICT, fp)
+else:
+    with open('color.json', 'r') as fp:
+        COLORS_DICT = json.load(fp)
+
+
+
+''' Getting info online'''
+ROUTES = fetch_routes()     # all routes
+ROUTE_STOP_DICT, STOPS = fetch_stops()  # stops of each routes and consolidated stop lists
+
+with open('route.json', 'w') as fp:
+    json.dump(ROUTES, fp)
+
+with open('route-stop.json', 'w') as fp:
+    json.dump(ROUTE_STOP_DICT, fp)
+
+with open('stop.json', 'w') as fp:
+    json.dump(STOPS, fp)
 
 
